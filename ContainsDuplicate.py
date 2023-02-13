989937b7abebaabb62baa5d4fbe429fe7043517a
T O M I. Tech <65241457+tomisi@users.noreply.github.com>
2023-02-13 06:39:09 +0200

    Update ContainsDuplicate.py

--- ContainsDuplicate.py
+++ ContainsDuplicate.py
@@ -8,7 +8,7 @@ class Solution(object):
     # For each element n in the nums list, the method checks whether the element is already in the hashset using the in    operator:
             if n in hashset:
     #If the element n is in the hashset, then the method returns True, indicating that there is a duplicate element in the nums list.
-# If the element n is not in the hashset, then the method adds the element to the hashset using the add() method:
+    # If the element n is not in the hashset, then the method adds the element to the hashset using the add() method:
                 return True  # if we find a duplicate element, return True
             hashset.add(n)  # add the current element to the hashset
         return False  # if we reach this point, there are no duplicates in the lis
